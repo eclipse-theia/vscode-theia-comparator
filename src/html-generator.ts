cfbb6985c26219bab2db349014ec1d2bf78805c1
Lucas Koehler <lkoehler@eclipsesource.com>
2022-01-18 16:09:02 +0100

    Add initial issue links
    * Add initial issue links as notes based on https://github.com/eclipse-theia/theia/issues/9863.
    * Tweak generated html code to open clicked links in a new tab/window and improve link color for readability
    
    Contributed on behalf of STMicroelectronics
    
    Signed-off-by: Lucas Koehler <lkoehler@eclipsesource.com>

--- src/html-generator.ts
+++ src/html-generator.ts
@@ -29,6 +29,10 @@ export class HTMLGenerator {
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<base target="_blank">
+<style>
+a { color: #0056b3; }
+</style>
 </head>
 <body>
 <div class="container-fluid">
