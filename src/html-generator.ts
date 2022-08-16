94f429ba5bc6703cf66f8dceb9c103e5e93f9639
Colin Grant <colin.grant@ericsson.com>
2022-08-17 00:40:49 +0200

    Combine reports

--- src/html-generator.ts
+++ src/html-generator.ts
@@ -43,24 +43,36 @@ class Tag implements Renderable {
     }
 }
 
-class Div extends Tag {
+class TR extends Tag {
     constructor(properties: Record<string, string> = Object.create(null), ...children: Renderable[]) {
-        super('div', properties, ...children);
+        super('tr', properties, ...children);
     }
 }
 
-class Row extends Div {
+class TD extends Tag {
+    constructor(properties: Record<string, string> = Object.create(null), ...children: Renderable[]) {
+        super('td', properties, ...children);
+    }
+}
+
+class TH extends Tag {
+    constructor(properties: Record<string, string> = Object.create(null), ...children: Renderable[]) {
+        super('th', properties, ...children);
+    }
+}
+
+class Row extends TR {
     constructor(name: string, complex: boolean, ...children: Renderable[]) {
         const text = new TextNode(name);
-        const props = { class: complex ? 'col-4 command' : 'col-4' };
-        const firstColumn = new Div(props, complex ? new Tag('b', {}, text) : new Div({ style: 'padding-left: 16px;' }, text));
-        super({ class: 'row bg-info' }, firstColumn, ...children);
+        const props = complex ? { class: 'command' } : { style: 'padding-left: 16px;' };
+        const firstColumn = new TD(props, complex ? new Tag('b', {}, text) : text);
+        super({ class: 'bg-info' }, firstColumn, ...children);
     }
 }
 
-class Status extends Div {
+class Status extends TD {
     constructor(statusColor: string, statusEmoji: string) {
-        super({ class: `col bg-${statusColor}`, style: 'max-width: 90px;' }, new TextNode(statusEmoji));
+        super({ class: `bg-${statusColor}` }, new TextNode(statusEmoji));
     }
     override appendChild(): void { }
     override appendChildren(): void { }
@@ -68,8 +80,8 @@ class Status extends Div {
 
 const CORRECT = new Status('success', '✔️');
 const ABSENT = new Status('danger', '✖️');
-const INCORRECT = new Status('warning', '✖️');
-const NOT_APPLICABLE = new Div({ class: 'col bg-light', style: 'max-width: 90px;' }, new TextNode('N/A'));
+const INCORRECT = new Status('warning', '⚠️');
+const NOT_APPLICABLE = new TD({ class: 'bg-light' }, new TextNode('N/A'));
 const EXPECTED = new Status('info', '✔️');
 
 export class HTMLGenerator {
@@ -94,22 +106,46 @@ a { color: #0056b3; }
 </style>
         `));
         const body = new Tag('body');
-        const pageContainer = new Div({ class: 'container-fluid' });
+        const table = new Tag('table');
+        const thead = new Tag('thead');
+        const tbodyFull = new Tag('tbody', { id: 'full-report' });
+        const tbodyFiltered = new Tag('tbody', { id: 'filtered-report', style: 'display: none;' });
+        table.appendChildren(thead, tbodyFull, tbodyFiltered);
         html.appendChildren(head, body);
-        body.appendChild(pageContainer);
-        const topRowProps = { class: 'col ide', style: 'max-width: 90px;' };
-        const namespaceRow = (name: string) => new Div({ class: 'row bg-warning' },
-            new Div({ class: 'col-4' }, new TextNode(`namespace/${name}`)),
-            ...new Array(totalColumns).fill(new Div({ class: 'col', style: 'max-width: 90px;' }))
+        body.appendChild(table);
+        body.appendChild(new TextNode(`
+    <script>
+        const form = document.getElementById('report-filter-selector-form');
+        const fullReport = document.getElementById('full-report');
+        const filteredReport = document.getElementById('filtered-report');
+        const radioButtons = document.getElementsByClassName('report-filter-selector');
+        Array.from(radioButtons).forEach(button => button.addEventListener('change', () => {
+            if (form['report-filter-selector'].value === 'filtered') {
+                fullReport.style.display = 'none';
+                filteredReport.style.display = '';
+            } else {
+                filteredReport.style.display = 'none';
+                fullReport.style.display = '';
+            }
+        }));
+    </script>
+        `))
+        const topRowProps = { class: 'ide' };
+        const namespaceRow = (name: string) => new TR({ class: 'bg-warning' },
+            new TD({ colspan: totalColumns.toString() }, new TextNode(`namespace/${name}`)),
         );
-        const firstRow = new Div({ class: 'row bg-primary', style: 'position: sticky; top: 0; z-index: 1000' },
-            new Div({ class: 'col-4 command' }, new TextNode('&nbsp;')),
-            ...Object.keys(comparisons.theia).map(version => new Div(topRowProps, new TextNode(`Theia ${version}`))),
-            new Div(topRowProps, new TextNode(`VSCode ${comparisons.vscodeReferenceVersion}`)),
-            ...Object.keys(comparisons.vscode).map(version => new Div(topRowProps, new TextNode(`VSCode ${version}`))),
+        const firstRow = new TR({ class: 'bg-primary', style: 'position: sticky; top: 0; z-index: 1000' },
+            new TH({ class: 'command' },
+                new Tag('form', { id: 'report-filter-selector-form', style: 'display: flex; justify-content: space-around; align-items: center; flex-flow: row nowrap; height: 100%; width: 100%;' },
+                    new Tag('label', { for: 'full-report-button' }, new TextNode('Full'), new Tag('input', { type: 'radio', class: 'report-filter-selector', name: 'report-filter-selector', id: 'full-report-button', value: "full", checked: 'true', style: 'margin-left: 8px;' })),
+                    new Tag('label', { for: 'filtered-report-button' }, new TextNode('Filtered'), new Tag('input', { type: 'radio', class: 'report-filter-selector', name: 'report-filter-selector', id: 'filtered-report-button', value: "filtered", style: 'margin-left: 8px;' })),
+                )),
+            ...Object.keys(comparisons.theia).map(version => new TH(topRowProps, new TextNode(`Theia ${version}`))),
+            new TH(topRowProps, new TextNode(`VSCode ${comparisons.vscodeReferenceVersion}`)),
+            ...Object.keys(comparisons.vscode).map(version => new TH(topRowProps, new TextNode(`VSCode ${version}`))),
         );
-        pageContainer.appendChild(firstRow);
-        const namespaces = new Array<Renderable[]>([namespaceRow('root')]);
+        thead.appendChild(firstRow);
+        const namespaces = new Array<[Renderable[], Renderable[]]>([[namespaceRow('root')], [namespaceRow('root')]]);
         const theiaColumn = (value: boolean | undefined | null | Comparison): Renderable => {
             switch (value) {
                 case undefined:
@@ -126,19 +162,25 @@ a { color: #0056b3; }
                 default: return EXPECTED;
             }
         };
-        const traverse = (current: Comparison, rows: Renderable[], ...pathSegments: string[]): Array<boolean | null> => {
+        const traverse = (current: Comparison, rows: Renderable[], filteredRows: Renderable[], ...pathSegments: string[]): Array<boolean | null> => {
             const success: Array<boolean | null | undefined> = new Array(totalColumns - 2).fill(undefined);
             Object.keys(current).sort().forEach(key => {
                 const value = current[key];
                 const newPathSegments = [...pathSegments, key];
                 if (value && typeof value === 'object') {
                     if (key.charAt(0).toLowerCase() === key.charAt(0)) { // Probably a namespace
-                        const namespaceRows = [namespaceRow(key)];
-                        traverse(value, namespaceRows, ...newPathSegments);
+                        const namespaceLabel = namespaceRow(key);
+                        const namespaceRenderables: [Renderable[], Renderable[]] = [[namespaceLabel], [namespaceLabel]];
+                        namespaces.push(namespaceRenderables)
+                        traverse(value, namespaceRenderables[0], namespaceRenderables[1], ...newPathSegments);
                     } else {
                         const complexRow = new Row(key, true);
                         rows.push(complexRow);
-                        const rowSuccess = traverse(value, rows, ...newPathSegments);
+                        const numFilteredRowsBeforeTraversal = filteredRows.push(complexRow);
+                        const rowSuccess = traverse(value, rows, filteredRows, ...newPathSegments);
+                        if (filteredRows.length === numFilteredRowsBeforeTraversal) { // No child rows were added - we should remove the complex row.
+                            filteredRows.pop();
+                        }
                         rowSuccess.forEach((aggregateSuccess, index) => {
                             if (index < numTheiaVersions) {
                                 // Empty interfaces will leave the value undefined.
@@ -153,9 +195,11 @@ a { color: #0056b3; }
                         });
                     }
                 } else {
+                    let appearsInFiltered = false;
                     const entry = new Row(key, false);
                     Object.values(comparisons.theia).forEach((version, index) => {
                         const valueAtLocation = retrieveValue(version.full, newPathSegments);
+                        appearsInFiltered ||= valueAtLocation === null || valueAtLocation === false;
                         updateSuccessValue(success, index, (valueAtLocation && typeof valueAtLocation === 'object') ? false : valueAtLocation as boolean | null);
                         entry.appendChild(theiaColumn(valueAtLocation));
                     });
@@ -167,6 +211,9 @@ a { color: #0056b3; }
                     });
 
                     rows.push(entry);
+                    if (appearsInFiltered) {
+                        filteredRows.push(entry);
+                    }
                 }
             });
             return success;
@@ -183,8 +230,11 @@ a { color: #0056b3; }
             }
         };
 
-        traverse(Object.values(comparisons.theia)[0].full, namespaces[0]);
-        namespaces.forEach(namespaceContents => pageContainer.appendChildren(...namespaceContents));
+        traverse(Object.values(comparisons.theia)[0].full, ...namespaces[0]);
+        namespaces.forEach(([namespaceContents, filteredContents]) => {
+            tbodyFull.appendChildren(...namespaceContents);
+            tbodyFiltered.appendChildren(...filteredContents);
+        });
         return html.render();
     }
 
